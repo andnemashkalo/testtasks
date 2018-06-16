c53180d55cad97e0069c5ad70fffeb63ff7f30db
andnemashkalo <alfonso8and@gmail.com>
2018-06-16 14:24:23 +0300

    test tasks

--- src/com/Practice_Generics/Stats.java
+++ src/com/Practice_Generics/Stats.java
@@ -12,7 +12,7 @@ public class Stats<T extends Number> {
             sum +=nums[i].doubleValue();
         return sum/nums.length;
     }
-    boolean sameAvg(Stats<?>ob){
+    boolean sameAvg(Stats<?>ob){// для приема любого типа значений
         if(average()==ob.average())
             return true;
 
