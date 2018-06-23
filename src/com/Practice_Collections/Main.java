28df54e2b459d4b18adf964db256f81d06b73372
andnemashkalo <alfonso8and@gmail.com>
2018-06-23 13:59:12 +0300

    test tasks

--- src/com/Practice_Collections/Main.java
+++ src/com/Practice_Collections/Main.java
@@ -1,7 +1,19 @@
 package com.Practice_Collections;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+import static java.util.Arrays.asList;
+
+public class Main {
+    /*static ArrayList<String> list=new ArrayList<String>(){{
+        add("aa");
+        add("bb");
+        add("cc");
+        add("dd");}};*/
+    static LinkedList<String> list = new LinkedList<String>(asList("cc","aa","bb"));
+
+    public static void main(String[] args) {
+        System.out.println(list);
     }
 }
